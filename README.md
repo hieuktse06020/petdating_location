967b14849c170518a3c068bc470ff7e9fefcb4d8
hieukt06020 <khuongtrunghieu38@gmail.com>
2020-07-20 21:53:21 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# petdating_location" 
---
+++ index.js
@@ -0,0 +1,9 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import App from './App';
+import {name as appName} from './app.json';
+import a from './screens/nearBy'
+AppRegistry.registerComponent(appName, () => a);
